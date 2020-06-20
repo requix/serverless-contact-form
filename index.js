06ea2e0a6bf44be9a7a74b44cc3fac7634758e1c
Volodymyr Marynychev <requix@gmail.com>
2020-06-20 22:26:14 +0200

    Save new submitted data to S3

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var sns = new AWS.SNS();
 
 var snsTopic = process.env.NEW_CONTACT_TOPIC;
 
+const s3 = new AWS.S3();
+const STORAGE_BUCKET_NAME = process.env.STORAGE_BUCKET_NAME;
 
 const httpResponse = (status, message) => {
     return {
@@ -18,11 +20,34 @@ const httpResponse = (status, message) => {
             "Access-Control-Allow-Headers": "'x-requested-with'"
         },
     };
-}
+};
+
+saveToS3 = (data, id) => {
+    const date = new Date(),
+        year = date.getFullYear(),
+        month = date.getMonth() + 1,
+        day = date.getDate(),
+        hash = id;
+
+    return s3.putObject({
+        Key: `${year}-${month}-${day}-${hash}`,
+        Bucket: STORAGE_BUCKET_NAME,
+        ContentType: 'application/json',
+        Body: JSON.stringify(data, null, 2)
+    });
+};
 
 exports.lambdaHandler = async (event, context) => {
     const body = JSON.parse(event.body);    
     console.log("Parsed body", body);
+
+    try {
+        await saveToS3(message, context.awsRequestId).promise();
+        console.log("Data saved to S3 bucket");
+    } catch (err) {
+        console.log("S3 Error: ", err);
+        return httpResponse(500, "KO");
+    }
 
     try {
         await sns.publish({
